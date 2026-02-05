a3a5da397c0b2ce0466ff21a9c60230f5bc74a1b
Alexander Creznic <alexander.creznic@protonmail.com>
2026-02-05 08:08:10 -0500

    removing old dead config file from project

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -2,14 +2,18 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
+    # App
     app_name: str = "Appointment Reminder API"
     environment: str = Field(default="dev")
 
+    # mongo
     mongo_uri: str = Field(..., description="MongoDB connection string")
     mongo_db_name: str = Field(default="reminder")
 
+    # logging
     log_level: str = Field(default="INFO")
 
+    # reading env configs
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
